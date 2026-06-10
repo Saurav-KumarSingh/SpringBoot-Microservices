c41fe5a2bb653a5ffbafac790b1df01fe240bd4b
Saurav-KumarSingh <sauravkumarsingh093@gmail.com>
2026-06-10 22:09:58 +0530

    Implementing Circuit Breaker using Resilience4J

--- user-service/src/main/java/microservices/user_service/controller/UserController.java
+++ user-service/src/main/java/microservices/user_service/controller/UserController.java
@@ -1,5 +1,6 @@
 package microservices.user_service.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import microservices.user_service.Dto.UserInformation;
 import microservices.user_service.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,14 +21,39 @@ public class UserController {
         return userService.createUser(userInformation);
     }
 
+    @CircuitBreaker(name = "orderCircuitBreaker", fallbackMethod = "orderFallBack")
     @GetMapping("/{email}")
     public Optional<UserInformation> getUser(@PathVariable String email){
 
         return userService.getUser(email);
     }
 
+
+    @CircuitBreaker(name = "orderCircuitBreaker", fallbackMethod = "getAllUserFallback")
     @GetMapping
     public List<UserInformation> getAllUser(){
         return userService.getAllUser();
+    }
+
+    //    creating fall back for circuit breaker
+
+    public Optional<UserInformation> orderFallBack(String email,Exception ex){
+        System.out.println("Fallback triggered because: " + ex.getMessage());
+
+        UserInformation user = new UserInformation();
+        user.setEmail(email);
+        user.setName("Service Unavailable");
+
+        return Optional.of(user);
+    }
+
+    public List<UserInformation> getAllUserFallback(Exception ex) {
+        System.out.println("Fallback triggered: " + ex.getMessage());
+
+        UserInformation user = new UserInformation();
+        user.setName("Service Unavailable");
+        user.setEmail("N/A");
+
+        return List.of(user);
     }
 }
